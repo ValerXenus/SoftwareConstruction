523b049b92d8547956cdb2ebe544ad9e6ac34588
ValerXenus <valeraxenus@mail.ru>
2021-07-04 16:39:41 +0300

    Fix for accidentally ignored files

---
+++ Kpo4162_nmv.Lib/source/Log/ErrorLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Kpo4162_nmv.Lib.source.Log
+{
+    /// <summary>
+    /// Класс для логгирования ошибок
+    /// </summary>
+    public static class ErrorLogger
+    {
+        /// <summary>
+        /// Логгирование ошибки
+        /// </summary>
+        /// <param name="errorText">Текст ошибки</param>
+        public static void LogError(string errorText)
+        {
+            var message = $"CAUGHT ERROR | {DateTime.Now:dd.MM.yyyy HH:ss} | {errorText}\n";
+            writeLogToFile(message);
+        }
+
+        /// <summary>
+        /// Запись лога в файл
+        /// </summary>
+        /// <param name="message">Текст лога</param>
+        private static void writeLogToFile(string message)
+        {
+            using (var stream = new FileStream("errorLogger.txt", FileMode.Append))
+            {
+                var stringContent = Encoding.UTF8.GetBytes(message);
+                stream.Write(stringContent, 0, stringContent.Length);
+            }
+        }
+    }
+}
---
+++ Kpo4162_nmv.Lib/source/Log/LogUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Kpo4162_nmv.Lib
+{
+    public static class LogUtility
+    {
+        /// <summary>
+        /// Запись ошибки в лог
+        /// </summary>
+        /// <param name="message"></param>
+        public static void ErrorLog(string message)
+        {
+            var errorMessage = $"{DateTime.Now:dd.MM.yyyy HH:ss} | {message}\n";
+            File.AppendAllText("error.log", errorMessage);
+        }
+
+        /// <summary>
+        /// Запись ошибки в лог по тексту исключения
+        /// </summary>
+        /// <param name="exception"></param>
+        public static void ErrorLog(Exception exception)
+        {
+            var errorMessage = $"{DateTime.Now:dd.MM.yyyy HH:ss} | {exception.Message}\n";
+            File.AppendAllText("error.log", errorMessage);
+        }
+    }
+}
